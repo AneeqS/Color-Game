c0ea6c90e79ef759079362d76f7c2c0817bbd937
Aneeq Shah <aneeqshah27gmail.com>
2019-01-11 21:31:16 -0500

    added difficulty options

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-var colors = generateColors(6);
+var numSquare = 6;
+var colors = generateColors(numSquare);
 var h1 = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var answerDisplay = document.querySelector("#answerDisplay");
 var newButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
+
+easyButton.addEventListener("click", function () {
+
+    hardButton.classList.remove("selected");
+    easyButton.classList.add("selected");
+
+    numSquare = 3;
+    colors = generateColors(numSquare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }else{
+            squares[i].style.display = "none";
+        }
+    }
+
+});
+
+hardButton.addEventListener("click", function () {
+
+    hardButton.classList.add("selected");
+    easyButton.classList.remove("selected");
+
+    numSquare = 6;
+    colors = generateColors(numSquare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
 
 newButton.addEventListener("click", function () {
 
     newButton.textContent = "New Colors?";
-    colors = generateColors(6);
+    colors = generateColors(numSquare);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i = 0; i < squares.length; i++){
